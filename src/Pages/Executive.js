a230669e478dbfb7e11760c31b7ffde1d1ad0294
cmajor22 <48356053+cmajor22@users.noreply.github.com>
2021-08-04 21:32:27 -0400

    Added executive and president's message

--- src/Pages/Executive.js
+++ src/Pages/Executive.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { Box, Card, makeStyles, Typography } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
 
 const useStyles = makeStyles((theme) => ({
-    
+    personCard: {
+        width: '200px',
+        padding: '5px',
+        display: 'flex'
+    },
+    personCardIcon: {
+        fontSize: '50px'
+    },
+    personCardContent: {
+        paddingLeft: '7px'
+    }
 }));
+
+const personList = [
+    {name: "Tim Osmund", title: "President"},
+    {name: "Doug Williamson", title: "Executive"},
+    {name: "Dave Newson", title: "Executive"},
+    {name: "Casey Major", title: "Executive"},
+]
 
 function Executive(props) {
     const classes = useStyles();
 
-    return <div>
-        Executive
-    </div>
+    return <Fragment>
+        {
+            personList.map((person) => {
+                return [
+                    <Card className={classes.personCard}>
+                        <Box>
+                            <PersonIcon className={classes.personCardIcon}></PersonIcon>
+                        </Box>
+                        <Box className={classes.personCardContent}>
+                            <Typography variant="subtitle1">{person.name}</Typography>
+                            <Typography variant="subtitle2">{person.title}</Typography>
+                        </Box>
+                    </Card>,
+                    <br />]
+            })
+        }
+    </Fragment>
 }
 
 export default Executive;
